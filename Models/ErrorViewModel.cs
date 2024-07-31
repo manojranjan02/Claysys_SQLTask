da9f85193c18dda8b3746b508fb2f651c63e4502
Manoj <manoj1260555@gmail.com>
2024-07-31 14:29:49 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Claysys_SQLTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
